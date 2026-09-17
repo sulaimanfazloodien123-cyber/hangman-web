ab61a1c00adf14fc75ad9f9ec61aab663a9b8d3b
sulaimanfazloodien123-cyber <sulaiman.fazloodien123@gmail.com>
2026-09-17 18:55:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,9 @@ const categories = {
          'darkseid', 'brainiac', 'shazam', 'raven', 'starfire', 'beastboy',
          'deathstroke', 'scarecrow', 'poisonivy', 'misterfreeze', 'clayface',
          'manbat', 'killercroc', 'oracle', 'huntress', 'zatanna',
-         'constantine', 'rorschach', 'nightowl', 'ozymandias']
+         constantine', 'rorschach', 'nightowl', 'ozymandias']
+};
+
 const stages = [
 `  +---+
   |   |
